e915d6b2d020fa7906728f88e1625703d657aec9
Rubin Peci <rubinpeci@gmail.com>
2019-02-14 12:35:23 -0500

    Only works for square matrices

--- main.c
+++ main.c
@@ -12,22 +12,26 @@ int main() {
   screen s;
   struct matrix *edges;
 
+  //put some values in edges to see that it works
   edges = new_matrix(4, 5);
   edges->m[0][0] = 1.1;
   edges->m[1][0] = 1.1;
 
+  //prints it out
   printf("Testing print_matrix\n");
   print_matrix(edges);
 
+  //make test an identity matrix
   struct matrix *test = new_matrix(5, 5);
   printf("\nTesting ident\n");
   ident(test);
   print_matrix(test);
-
+  
+  //free test matrices
+  free_matrix(test);
   free_matrix( edges );
-  free_matrix( test );
-
-  struct matrix *uwu = new_matrix(3, 3);
+  
+  struct matrix *uwu = new_matrix(4, 3);
   int tmp = 0;
 
   for(int rows = 0; rows < uwu->rows; rows++)
@@ -41,7 +45,7 @@ int main() {
   printf("\nPrinting matrix uwu\n");
   print_matrix(uwu);
  
-  struct matrix *owo = new_matrix(3, 3);
+  struct matrix *owo = new_matrix(3, 4);
   tmp = 0;
 
   for(int rows = 0; rows < owo->rows; rows++)
@@ -61,4 +65,10 @@ int main() {
   printf("\nPrinting uwu * owo\n");
   print_matrix(owo);
 
+  struct matrix *identity = new_matrix(3, 3);
+  ident(identity);
+
+  matrix_mult(owo, identity);
+  printf("\nTesting multiplication with ident\nowo * IDENTITY\n");
+  print_matrix(identity); 
 } 
